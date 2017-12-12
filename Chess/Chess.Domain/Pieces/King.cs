46753fd0e6de66d30cac97e5c1d51221a6308482
lafsar <nihlanth@gmail.com>
2017-12-11 18:00:33 -0700

    Add other pieces

---
+++ Chess/Chess.Domain/Pieces/King.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chess.Domain.Pieces
+{
+	public sealed class King : ChessPiece
+	{
+		public King(PieceColor color) : base(color) { }
+		public override IEnumerable<Tuple<int, int>> PossibleStartingPositions()
+		{
+			return new List<Tuple<int, int>>().AsEnumerable();
+		}
+	}
+}
---
+++ Chess/Chess.Domain/Pieces/Queen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chess.Domain.Pieces
+{
+	public sealed class Queen : ChessPiece
+	{
+		public Queen(PieceColor color) : base(color) { }
+		public override IEnumerable<Tuple<int, int>> PossibleStartingPositions()
+		{
+			return new List<Tuple<int, int>>().AsEnumerable();
+		}
+	}
+}
